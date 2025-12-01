bbb2b42577a9a610b0207a092d6737e7c9ad95a0
TAMAL NAYEK <tamal.nayek@yahoo.com>
2025-12-01 10:16:42 +0530

    refactor: Apply discount upto 5 distinct books in more optimized way. Refactor code, remove if-else conditions and apply more genric way to calculate discount. Closes #7

--- src/main/java/com/bnpp/kata/book/price/service/BookDiscountService.java
+++ src/main/java/com/bnpp/kata/book/price/service/BookDiscountService.java
@@ -4,28 +4,26 @@ import com.bnpp.kata.book.price.model.Book;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class BookDiscountService {
 
     private static final double BOOK_PRICE = 50.0;
 
-
+    private static final Map<Integer, Double> DISCOUNTS = Map.of(
+            1, 0.00,
+            2, 0.05,
+            3, 0.10,
+            4, 0.20,
+            5, 0.25
+    );
 
     public double calculatePrice(List<Book> bookList) {
         int reqBookCount = bookList.size();
-        double totalPrice = 0.0;
-        if(reqBookCount == 1) {
-            totalPrice = BOOK_PRICE * reqBookCount;
-        }else if(reqBookCount == 2){
-            totalPrice = (BOOK_PRICE * reqBookCount) * (1 - 0.05);
-        }else if(reqBookCount == 3){
-            totalPrice = (BOOK_PRICE * reqBookCount) * (1 - 0.10);
-        }else if(reqBookCount == 4){
-            totalPrice = (BOOK_PRICE * reqBookCount) * (1 - 0.20);
-        }else if(reqBookCount == 5){
-            totalPrice = (BOOK_PRICE * reqBookCount) * (1 - 0.25);
-        }
+        double discount = DISCOUNTS.getOrDefault(reqBookCount, 0.0);
+        double totalPrice;
+        totalPrice = (BOOK_PRICE * reqBookCount) * (1 - discount);
         return totalPrice;
     }
 
